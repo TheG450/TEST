fffe3a49b7deac94084bd04642cad5b91e21816e
TheG450 <120777134+TheG450@users.noreply.github.com>
2023-06-07 13:34:38 +0700

    Update TEST1.lua

--- TEST1.lua
+++ TEST1.lua
@@ -1,6 +1,6 @@
 repeat wait(3) until game:IsLoaded()
 local key = _G.Key
-local keys = 
+local keys = loadstring(game:HttpGet("https://raw.githubusercontent.com/TheG450/MakeItsPaidVIP/main/Paid.lua"))()
 if game.PlaceId == 11481193418 then
     if game:GetService("Players").LocalPlayer.Name == "thebigblockclock1" and game:GetService("Players").LocalPlayer.UserId == 4135685309 then
         if table.find(keys, key) then
